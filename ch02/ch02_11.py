f1d66d853067cd10e41b79201b5d75f7aff0e703
seiya8 <seiya.111122@gmail.com>
2022-11-13 16:40:30 +0900

    solve 10 and 11

---
+++ ch02/ch02_11.py
@@ -0,0 +1,4 @@
+with open('ch02_11_output.txt', 'w') as output_f:
+  with open('popular-names.txt', 'r') as input_f:
+    for line in input_f:
+      output_f.write(line.replace('\t', ' '))
---
+++ ch02/ch02_11.sh
@@ -0,0 +1,2 @@
+expand -t 1 popular-names.txt
+# tr '\t' ' ' < popular-names.txt
