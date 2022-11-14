881ecc11af1d23218f277c4d2c34a901defbc871
seiya8 <seiya.111122@gmail.com>
2022-11-14 19:41:37 +0900

    change variable names

--- ch02/ch02_12.rb
+++ ch02/ch02_12.rb
@@ -1,9 +1,9 @@
-File.open('col1.txt', 'w') do |col1_f|
-  File.open('col2.txt', 'w') do |col2_f|
-    File.foreach('popular-names.txt') do |line|
-      col1, col2, _ = line.split(/\t/)
-      col1_f.puts(col1)
-      col2_f.puts(col2)
+File.open('col1.txt', 'w') do |col1_line|
+  File.open('col2.txt', 'w') do |col2_line|
+    File.foreach('popular-names.txt') do |in_line|
+      col1, col2, _ = in_line.split(/\t/)
+      col1_line.puts(col1)
+      col2_line.puts(col2)
     end
   end
 end
